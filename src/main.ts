16d71d5916af57a855aa10e7b5291e5ed7095c01
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-06-09 11:54:43 -0300

    Forzar build limpio eliminando dist antes de compilar

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { Reflector }    from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import 'reflect-metadata';
+
 
 
 async function bootstrap() {
