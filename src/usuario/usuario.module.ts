8d71c935a9c4d1b1af4a56b943b943ab1724755b
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-07-06 11:56:33 -0300

    Fixes fran

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
-
+import { UsuarioRol } from '../usuario-rol/usuario-rol.entity';
+import { Role } from '../role/role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
+  imports: [
+    TypeOrmModule.forFeature([Usuario, UsuarioRol, Role]) // 👈 agregamos los repos necesarios
+  ],
   providers: [UsuarioService],
   controllers: [UsuarioController],
   exports: [UsuarioService],
