4c485de3ad890e9a00b1e1b54ff875ece33d85be
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-09-04 15:16:20 -0300

    Precios por almacen

--- src/stock-actual/stock-actual.module.ts
+++ src/stock-actual/stock-actual.module.ts
@@ -4,9 +4,10 @@ import { StockActualService } from './stock-actual.service';
 import { StockActualController } from './stock-actual.controller';
 import { StockActual } from './stock-actual.entity';
 import { MovimientoStockModule } from 'src/movimiento-stock/movimiento-stock.module';
+import { ProductoPrecioAlmacen } from 'src/producto-precio-almacen/producto-precio-almacen.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StockActual]),MovimientoStockModule],
+  imports: [TypeOrmModule.forFeature([StockActual,ProductoPrecioAlmacen]),MovimientoStockModule],
   providers: [StockActualService],
   controllers: [StockActualController],
   exports: [ StockActualService ],
