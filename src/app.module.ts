4b23d54164c238f47ed2fb2dd7adbc2203be7600
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-09-04 12:30:11 -0300

    Precios por almacen

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,16 +27,13 @@ import { FacturaModule } from './factura/factura.module';
 import { GastosModule } from './gastos/gastos.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }),
+   imports: [ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +(process.env.DB_PORT || 5432),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL, // ✅ Usar DATABASE_URL de Railway
   autoLoadEntities: true,
   synchronize: true,
+  ssl: { rejectUnauthorized: false },
 }),
     CategoriaModule,
     ProveedorModule,
