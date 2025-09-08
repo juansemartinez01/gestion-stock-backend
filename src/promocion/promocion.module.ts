fe068a6c9323fc0b260ca4250c6c3788664c2777
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-09-08 12:43:21 -0300

    Arreglos gramos

--- src/promocion/promocion.module.ts
+++ src/promocion/promocion.module.ts
@@ -4,9 +4,10 @@ import { Promocion } from './promocion.entity';
 import { PromocionProducto } from './promocion-producto.entity';
 import { PromocionService } from './promocion.service';
 import { PromocionController } from './promocion.controller';
+import { Producto } from 'src/producto/producto.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Promocion, PromocionProducto])],
+  imports: [TypeOrmModule.forFeature([Promocion, PromocionProducto,Producto])],
   providers: [PromocionService],
   controllers: [PromocionController],
   exports: [PromocionService],
